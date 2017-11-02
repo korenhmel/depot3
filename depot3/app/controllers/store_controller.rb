98e72c1236cfe7d767f9e1a69753eac6e4770484
koren <koren.hmel@gmail.com>
2017-11-02 15:13:00 +0200

    create session counter and add new column to line_items

--- depot3/app/controllers/store_controller.rb
+++ depot3/app/controllers/store_controller.rb
@@ -1,5 +1,20 @@
 class StoreController < ApplicationController
+  
+   
+
   def index
+    @set_count = set_count
     @products = Product.order(:title)
+  end
+
+    private
+  
+  def set_count
+    
+    if session[:counter].nil? 
+      session[:counter] = 0
+    end
+     session[:counter]+=1
+     
   end
 end
--- depot3/app/models/product.rb
+++ depot3/app/models/product.rb
@@ -17,11 +17,11 @@ class Product < ApplicationRecord
   # "убеждаемся что товарных позиций не существует"
 
   def ensure_not_referenced_by_any_line_item
-    if line_items.empty,
+    if line_items.empty?
      return true
     else
      errors.add(:base, 'существуют товарные позиции')
      return false
     end
   end
-end
+  end
